92e91f67374eb7dba6f29bb3aea1f8823b611b8f
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2016-07-21 13:38:03 -0300

    .get feito mais teste e atualizacao na classe Avaliador.

--- teste-de-unidade/src/br/com/leilao/servico/Avaliador.java
+++ teste-de-unidade/src/br/com/leilao/servico/Avaliador.java
@@ -1,22 +1,45 @@
 package br.com.leilao.servico;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import br.com.leilao.dominio.Lance;
 import br.com.leilao.dominio.Leilao;
 
 public class Avaliador {
 	private double maiorDeTodos = Double.MIN_VALUE;
-	private double menorDeTodos=Double.MAX_VALUE;
+	private double menorDeTodos = Double.MAX_VALUE;
+	private List<Lance> maiores;
 
 	public void avalia(Leilao leilao) {
 		for (Lance lance : leilao.getLances()) {
-			if (lance.getValor() > maiorDeTodos)	maiorDeTodos = lance.getValor();
-			if(lance.getValor()<menorDeTodos) menorDeTodos=lance.getValor();
+			if (lance.getValor() > maiorDeTodos)
+				maiorDeTodos = lance.getValor();
+			if (lance.getValor() < menorDeTodos)
+				menorDeTodos = lance.getValor();
 		}
+		// Os 3 maiores Valores de Lance.
+		maiores = new ArrayList<Lance>(leilao.getLances());
+		Collections.sort(maiores, new Comparator<Lance>() {
+			public int compare(Lance a1, Lance a2) {
+				if (a1.getValor() < a2.getValor())	return 1;
+				if (a1.getValor() > a2.getValor())	return -1;
+				return 0;
+			}
+		});
+		maiores = maiores.subList(0, maiores.size()> 3?3:maiores.size());
+	}
+
+	public List<Lance> getTresMaiores() {
+		return maiores;
 	}
 
 	public double getMaiorDeLance() {
 		return maiorDeTodos;
 	}
+
 	public double getMenorDeLance() {
 		return menorDeTodos;
 	}
--- teste-de-unidade/src/br/com/leilao/teste/TesteDoAvaliador.java
+++ teste-de-unidade/src/br/com/leilao/teste/TesteDoAvaliador.java
@@ -1,5 +1,9 @@
 package br.com.leilao.teste;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import br.com.leilao.dominio.Lance;
@@ -27,7 +31,45 @@ public class TesteDoAvaliador {
 		// 3 Valida
 		System.out.println(leiloeiro.getMaiorDeLance());
 		System.out.println(leiloeiro.getMenorDeLance());
-		Assert.assertEquals(50000.0, leiloeiro.getMaiorDeLance(), 0.0001);
-		Assert.assertEquals(10.0, leiloeiro.getMenorDeLance(), 0.0001);
+		assertEquals(50000.0, leiloeiro.getMaiorDeLance(), 0.0001);
+		assertEquals(10.0, leiloeiro.getMenorDeLance(), 0.0001);
+	}
+
+	@Test
+	public void deveEntenderLeilaoComApenasUmLance() {
+		Usuario jse = new Usuario("Jose");
+
+		Leilao leilao = new Leilao("Casa");
+
+		leilao.propoe(new Lance(jse, 1000));
+
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+
+		assertEquals(1000, leiloeiro.getMaiorDeLance(), 0.001);
+		assertEquals(1000, leiloeiro.getMenorDeLance(), 0.001);
+	}
+
+	@Test
+	public void deveEncontrarOsTresMaiores() {
+		Usuario mario = new Usuario("Mario");
+		Usuario jose = new Usuario("jose");
+
+		Leilao leilao = new Leilao("Casa");
+
+		leilao.propoe(new Lance(mario, 1000));
+		leilao.propoe(new Lance(jose, 2000));
+
+		leilao.propoe(new Lance(mario, 3000));
+		leilao.propoe(new Lance(jose, 4000));
+
+		Avaliador leiroeiro = new Avaliador();
+		leiroeiro.avalia(leilao);
+
+		List<Lance> maiores = leiroeiro.getTresMaiores();
+		assertEquals(3, maiores.size());
+		assertEquals(2000, maiores.get(2).getValor(), 0.0001);
+		assertEquals(3000, maiores.get(1).getValor(), 0.0001);
+		assertEquals(4000, maiores.get(0).getValor(), 0.0001);
 	}
 }
